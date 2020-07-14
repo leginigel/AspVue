97bbd83885a41a49cd9fa17032c7f4282fe8e25a
leginigel <leginigel@github.com>
2020-07-14 09:45:18 +0800

    start mvc https://docs.microsoft.com/zh-tw/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-3.1

--- Startup.cs
+++ Startup.cs
@@ -6,21 +6,38 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using AspVue.Data;
+using Microsoft.EntityFrameworkCore;
 namespace AspVue
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            {
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContextLocal"));
+                }
+                else
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext"));
+                }
+            });
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
@@ -29,8 +46,9 @@ namespace AspVue
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MvcMovieContext context)
         {
+            context.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
